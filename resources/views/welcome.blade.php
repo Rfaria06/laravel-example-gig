b56d6019a8ea6d2751c7f8e61438ec34bc9062de
Raul Faria <58264697+Rfaria06@users.noreply.github.com>
2024-04-23 00:40:25 +0200

    Blade templates basics

--- resources/views/welcome.blade.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{str_replace('_', '-', app()->getLocale())}}">
-
-<body>
-    <h1>Hi mom</h1>
-</body>
-
-</html>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,33 @@
 <?php
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
+// All listings
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
 });
 
-Route::get('/hello', function () {
-    return response('<h1>Hello World!</h1>')
-        ->header('Content-Type', 'application/json')
-        ->header('foo', 'bar');
+Route::get('/listings/{id}', function($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
+    ]);
 });
 
-Route::get('/posts/{id}', function ($id) {
-    return response('Post ' . $id);
-})->where('id', '[0-9]+');
-
-Route::get('/search', function (Request $request) {
-
-})
+// Route::get('/hello', function () {
+//     return response('<h1>Hello World!</h1>')
+//         ->header('Content-Type', 'application/json')
+//         ->header('foo', 'bar');
+// });
+//
+// Route::get('/posts/{id}', function ($id) {
+//     return response('Post ' . $id);
+// })->where('id', '[0-9]+');
+//
+// Route::get('/search', function (Request $request) {
+//     return $request->name . ' ' . $request->city;
+// });
