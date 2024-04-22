79e8d2c5fac5f69ca29ebb84c894a0ee17d18769
raul faria <raul@faria.ch>
2024-04-22 20:26:14 +0200

    testing routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return response('<h1>Hello World!</h1>')
+        ->header('Content-Type', 'application/json')
+        ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function ($id) {
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function (Request $request) {
+
+})
